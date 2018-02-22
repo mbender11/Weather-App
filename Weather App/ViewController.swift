d20421b9e44cf7e7ee8702755f87ed48057cf4c1
Mason Bender <bender_mason@yahoo.com>
2018-02-22 14:03:43 -0700

    Initial commit. Setup UI.

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var weather: UILabel!
+    
+    @IBAction func getWeather(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
